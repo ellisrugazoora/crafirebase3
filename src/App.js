c5343ef5add975efe86a69717d655a7bd2c6bacb
ellisrugazoora <ellisrgrz@gmail.com>
2024-11-04 16:54:39 +0300

    Added input and response display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [input, setInput] = useState('');
+  const [response, setResponse] = useState('');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello world! Good morning.
-          erflekjr
+          Edit <code>src/App.js</code> and save to reload. This confirms correspondence
         </p>
         <a
           className="App-link"
@@ -18,9 +22,26 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+      <div>
+        <p>{response}</p>
+      </div>
       </header>
     </div>
   );
+  function sendMessage(e){
+    console.log(e.target)
+    let res = "Response to: " + input;
+    setResponse(res)
+    console.log("Send message")
+  }
 }
 
 export default App;
